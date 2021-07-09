f8373c4a3f405db2de5596a9cf673e78d8b00ff8
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-07-08 19:23:02 -0600

    Finished

---
+++ The Reverse Cipher/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace The_Reverse_Cipher
+{
+    partial class frmReverseCipher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuote = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtEncrypt = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtDecrypt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblQuote
+            // 
+            this.lblQuote.AutoSize = true;
+            this.lblQuote.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuote.Location = new System.Drawing.Point(12, 38);
+            this.lblQuote.Name = "lblQuote";
+            this.lblQuote.Size = new System.Drawing.Size(767, 31);
+            this.lblQuote.TabIndex = 0;
+            this.lblQuote.Text = "\"Every man is surrounded by a neighborhood of voluntary spies.\" --Jane Austen, No" +
+    "rthanger Abby";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(38, 129);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(103, 25);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Enter Message:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(150, 133);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(628, 20);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(43, 178);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(87, 40);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.Location = new System.Drawing.Point(150, 188);
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.Size = new System.Drawing.Size(628, 20);
+            this.txtEncrypt.TabIndex = 4;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(43, 241);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(87, 40);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtDecrypt
+            // 
+            this.txtDecrypt.Location = new System.Drawing.Point(150, 252);
+            this.txtDecrypt.Name = "txtDecrypt";
+            this.txtDecrypt.Size = new System.Drawing.Size(628, 20);
+            this.txtDecrypt.TabIndex = 6;
+            // 
+            // frmReverseCipher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 325);
+            this.Controls.Add(this.txtDecrypt);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtEncrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblQuote);
+            this.Name = "frmReverseCipher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Reverse Cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuote;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtDecrypt;
+    }
+}
+
---
+++ The Reverse Cipher/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Reverse_Cipher
+{
+    public partial class frmReverseCipher : Form
+    {
+        public frmReverseCipher()
+        {
+            InitializeComponent();
+        }
+
+        string message;
+        string encrypted;
+        string decrypted;
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            message = txtMessage.Text;
+            int i = message.Length - 1;
+
+            while(i >= 0)
+            {
+                encrypted = encrypted + message[i];
+                i = i - 1;
+            }
+
+            txtMessage.Text = encrypted;
+            txtEncrypt.Text = encrypted;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            message = txtMessage.Text;
+            int i = message.Length - 1;
+
+            while (i >= 0)
+            {
+                decrypted = decrypted + message[i];
+                i = i - 1;
+            }
+
+            txtMessage.Text = decrypted;
+            txtDecrypt.Text = decrypted;
+        }
+    }
+}
